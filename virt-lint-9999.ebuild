zippy2/virt-lint

commit 8a6e4e23d75bd34b2c1076f659e7dacc378d5043

Introduce support for validators written in Lua
For the time being, I'm keeping both Rust and Lua validators.
I had to switch caps_cache to store Strings instead of parsed
documents, because:

  1) I'm ditching unmaintained sxd_document and sxd_xpath in
     favor of libxml,
  2) I'm working on Python bindings for which I need VirtLint
     struct to satisfy Send trait and neither sxd_document's
     Document, nor libxml's Document have that [1].

To avoid random test failures, warnings obtained via
VirtLint::warnings() are now sorted (readdir() and Lua and
stuff) which means that warning() method now has to take a
mutable reference to VirtLint object.

1: https://github.com/KWARC/rust-libxml/issues/123

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

virt-lint-9999.ebuild
@@ -12,6 +12,10 @@ CRATES="
 	anstyle-parse@0.2.1
 	anstyle-query@1.0.0
 	anstyle-wincon@2.1.0
+	autocfg@1.1.0
+	bstr@1.7.0
+	cc@1.0.83
+	cfg-if@1.0.0
 	clap@4.4.4
 	clap_builder@4.4.4
 	clap_derive@4.4.2
@@ -20,6 +24,11 @@ CRATES="
 	enum-display-derive@0.1.1
 	heck@0.4.1
 	libc@0.2.148
+	memchr@2.6.4
+	mlua@0.9.1
+	mlua-sys@0.3.2
+	num-traits@0.2.17
+	once_cell@1.18.0
 	peresil@0.3.0
 	pkg-config@0.3.27
 	pkg-version@1.0.0
@@ -28,6 +37,9 @@ CRATES="
 	proc-macro2@1.0.67
 	quick-error@1.2.3
 	quote@1.0.33
+	rustc-hash@1.1.0
+	serde@1.0.189
+	serde_derive@1.0.189
 	strsim@0.10.0
 	sxd-document@0.3.2
 	sxd-xpath@0.4.2
@@ -54,7 +66,7 @@ declare -A GIT_CRATES=(
 	[virt]="https://gitlab.com/libvirt/libvirt-rust.git;c7ee11c7585897f343d1c5ca9adc344bd9d21007;cargo_home/git/checkouts/libvirt-rust-a02c7639fc7cb7e7/c7ee11c/"
 )
 
-inherit edo cargo
+inherit edo cargo go-module
 
 DESCRIPTION="Virtualization linting library"
 HOMEPAGE="https://gitlab.com/MichalPrivoznik/virt-lint"
@@ -72,11 +84,16 @@ fi
 # use cargo-license for a more accurate license picture
 LICENSE="0BSD Apache-2.0 Apache-2.0-with-LLVM-exceptions BSD BSD-2 ISC LGPL-2.1 LGPL-3+ MIT Unicode-DFS-2016 Unlicense"
 SLOT="0"
+IUSE="+c +go"
+REQUIRED_USE="go? ( c )"
 
-DEPEND=""
+DEPEND="
+	dev-lang/lua:5.4
+	c? ( app-emulation/libvirt )"
 RDEPEND="${DEPEND}"
-BDEPEND="
-	dev-util/cargo-c"
+BDEPEND="${RDEPEND}
+	c? ( dev-util/cargo-c )
+	go? ( dev-lang/go )"
 
 # rust does not use *FLAGS from make.conf, silence portage warning
 # update with proper path to binaries this crate installs, omit leading /
@@ -103,7 +120,7 @@ src_compile() {
 
 	cargo_src_compile
 
-	edo cargo cbuild "${cargoargs[@]}"
+	use c && edo cargo cbuild "${cargoargs[@]}"
 }
 
 src_install() {
@@ -118,5 +135,7 @@ src_install() {
 
 	cargo_src_install --path ./tools
 
-	edo cargo cinstall "${cargoargs[@]}"
+	emake DESTDIR="${D}" install-data
+
+	use c && edo cargo cinstall "${cargoargs[@]}"
 }

Makefile
@@ -1,4 +1,6 @@
 VERSION ?= $(shell git describe --tags HEAD 2>/dev/null || echo "0.0.1")
+prefix ?= /usr
+datarootdir ?= $(prefix)/share
 
 all: rust c-build go-build
 
@@ -26,7 +28,7 @@ c-build: rust
 c-run: c-build
 	$(MAKE) -C tools/c/ run
 
-go-build: rust
+go-build: rust-cbuild
 	$(MAKE) -C go/
 
 go-run: go-build
@@ -69,3 +71,10 @@ rpm: virt-lint-$(VERSION).tar.xz virt-lint-$(VERSION)-vendor.tar.xz
 	cp virt-lint-$(VERSION)-vendor.tar.xz ~/rpmbuild/SOURCES && \
 	rpmbuild -ba virt-lint.spec && \
 	rm -f virt-lint-$(VERSION)-vendor.tar.xz
+
+install-data:
+	mkdir -p $(DESTDIR)$(datarootdir)/virt-lint/validators_lua
+	cp --recursive validators_lua $(DESTDIR)$(datarootdir)/virt-lint/
+
+uninstall-data:
+	rm -rf $(DESTDIR)$(datarootdir)/virt-lint/

go/Makefile
@@ -4,7 +4,10 @@ ifeq ($(VIRTLINT_BASE),)
 VIRTLINT_BASE := $(PWD)/target/x86_64-unknown-linux-gnu/debug/
 endif
 
-all: virt-lint-cli
+all: libvirt-mod virt-lint-cli
+
+libvirt-mod:
+	go mod download libvirt.org/go/libvirt
 
 virt-lint-cli: virt-lint-cli.go virt-lint/virt-lint.go $(VIRTLINT_BASE)/virt_lint.h
 	PKG_CONFIG_PATH=$(VIRTLINT_BASE) go build $<

go/virt-lint/virt-lint_test.go
@@ -4,10 +4,16 @@ package VirtLint_test
 
 import (
 	libvirt "libvirt.org/go/libvirt"
+	"os"
 	"reflect"
 	"testing"
 	"virt-lint/virt-lint"
 )
+
+func TestMain(t *testing.T) {
+	// TODO - switch to an absolute path, replaced at compile time
+	os.Setenv("VIRT_LINT_LUA_PATH", "../validators_lua:../../validators_lua")
+}
 
 func getConn(t *testing.T) *libvirt.Connect {
 	conn, err := libvirt.NewConnect("test:///default")
@@ -58,7 +64,9 @@ func TestListTags(t *testing.T) {
 		return
 	}
 
-	expect := []string{"TAG_1", "TAG_2", "TAG_3", "TAG_4"}
+	expect := []string{"TAG_1", "TAG_2", "TAG_3", "TAG_4",
+		"common", "common/check_node_kvm", "common/check_numa",
+		"common/check_numa_free", "common/check_pcie_root_ports"}
 	if !reflect.DeepEqual(tags, expect) {
 		t.Errorf("Tags don't match:\nexpected = %v\ngot = %v", expect, tags)
 		return
@@ -102,8 +110,14 @@ func TestSimple(t *testing.T) {
 	}
 
 	expect := []VirtLint.VirtLintWarning{
-		VirtLint.VirtLintWarning{[]string{"TAG_1", "TAG_2"}, VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
-		VirtLint.VirtLintWarning{[]string{"TAG_2"}, VirtLint.DOMAIN, VirtLint.ERROR, "Not enough free memory on any NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"TAG_1", "TAG_2"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"TAG_2"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Not enough free memory on any NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"common", "common/check_numa"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"common", "common/check_numa_free"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Not enough free memory on any NUMA node"},
 	}
 	if !reflect.DeepEqual(warn, expect) {
 		t.Errorf("Warnings don't match:\nexpected = %v\ngot = %v", expect, warn)
@@ -174,7 +188,10 @@ func TestOfflineSimple(t *testing.T) {
 	}
 
 	expect := []VirtLint.VirtLintWarning{
-		VirtLint.VirtLintWarning{[]string{"TAG_1", "TAG_2"}, VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"TAG_1", "TAG_2"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"common", "common/check_numa"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
 	}
 	if !reflect.DeepEqual(warn, expect) {
 		t.Errorf("Warnings don't match:\nexpected = %v\ngot = %v", expect, warn)
@@ -240,7 +257,8 @@ func TestOfflineWithError(t *testing.T) {
 	}
 
 	// This succeeds, because we deliberately run offline only validators
-	err = vl.Validate(domxml, []string{"TAG_1", "TAG_3", "TAG_4"}, true)
+	err = vl.Validate(domxml, []string{"TAG_1", "TAG_3", "TAG_4",
+		"common/check_node_kvm", "common/check_numa", "common/check_pcie_root_ports"}, true)
 	if err != nil {
 		t.Error(err)
 		return
@@ -253,7 +271,10 @@ func TestOfflineWithError(t *testing.T) {
 	}
 
 	expect := []VirtLint.VirtLintWarning{
-		VirtLint.VirtLintWarning{[]string{"TAG_1", "TAG_2"}, VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"TAG_1", "TAG_2"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
+		VirtLint.VirtLintWarning{[]string{"common", "common/check_numa"},
+			VirtLint.DOMAIN, VirtLint.ERROR, "Domain would not fit into any host NUMA node"},
 	}
 	if !reflect.DeepEqual(warn, expect) {
 		t.Errorf("Warnings don't match:\nexpected = %v\ngot = %v", expect, warn)

src/Cargo.toml
@@ -17,7 +17,7 @@ capi = []
 [dependencies]
 enum-display-derive = "0.1.1"
 pkg-version = "1.0.0"
-sxd-document = "0.3.2"
-sxd-xpath = "0.4.2"
+libxml = "0.3.3"
 thiserror = "1.0.48"
-virt={ git="https://gitlab.com/libvirt/libvirt-rust.git", rev="c7ee11c7585897f343d1c5ca9adc344bd9d21007" }
+virt = { git="https://gitlab.com/libvirt/libvirt-rust.git", rev="b0506ed9cca960dd5f7679bca434a19d4b5cfc1d" }
+mlua = { version="0.9.1", features=["lua54"] }

src/capi.rs
@@ -89,7 +89,8 @@ pub extern "C" fn virt_lint_capabilities_set(
         None
     } else {
         let c_str = unsafe { CStr::from_ptr(capsxml) };
-        Some(c_str.to_str().unwrap())
+        let c_string = String::from(c_str.to_str().unwrap());
+        Some(c_string)
     };
 
     if let Err(x) = vl.capabilities_set(new_capsxml) {
@@ -127,10 +128,10 @@ pub extern "C" fn virt_lint_domain_capabilities_add(
     check_not_null!(domcapsxml, err, -1);
 
     let vl = unsafe { &mut *vl };
-    let c_str = unsafe { CStr::from_ptr(domcapsxml) };
-    let domcapsxml = c_str.to_str().unwrap();
+    let domcapsxml_str = unsafe { CStr::from_ptr(domcapsxml) };
+    let domcapsxml_string = String::from(domcapsxml_str.to_str().unwrap());
 
-    if let Err(x) = vl.domain_capabilities_add(domcapsxml) {
+    if let Err(x) = vl.domain_capabilities_add(domcapsxml_string) {
         err_set(err, x);
         return -1;
     }
@@ -160,7 +161,7 @@ pub extern "C" fn virt_lint_validate(
     for i in 0..ntags {
         let t = unsafe { *tags.offset(i.try_into().unwrap()) };
         let t_str = unsafe { CStr::from_ptr(t) };
-        let t_string = t_str.to_str().unwrap().to_owned();
+        let t_string = String::from(t_str.to_str().unwrap());
         tags_vec.push(t_string);
     }
 
@@ -181,7 +182,14 @@ pub extern "C" fn virt_lint_list_tags(
 
     check_not_null!(tags, err, -1);
 
-    let mut v: Vec<_> = VirtLint::list_validator_tags()
+    let ret = VirtLint::list_validator_tags();
+    if let Err(x) = ret {
+        err_set(err, x);
+        return -1;
+    }
+
+    let mut v: Vec<_> = ret
+        .unwrap()
         .iter()
         .map(|s| CString::new(s.as_str()).unwrap().into_raw())
         .collect();
@@ -208,7 +216,7 @@ pub struct CVirtLintWarning {
 
 #[no_mangle]
 pub extern "C" fn virt_lint_get_warnings(
-    vl: *const VirtLint,
+    vl: *mut VirtLint,
     warnings: *mut *mut CVirtLintWarning,
     err: *mut *mut VirtLintError,
 ) -> isize {
@@ -217,7 +225,7 @@ pub extern "C" fn virt_lint_get_warnings(
     check_not_null!(vl, err, -1);
     check_not_null!(warnings, err, -1);
 
-    let vl = unsafe { &*vl };
+    let vl = unsafe { &mut *vl };
     let mut c_warn: Vec<CVirtLintWarning> = Vec::new();
 
     let warn = vl.warnings();

src/caps_cache.rs
@@ -2,12 +2,10 @@
 
 use crate::*;
 use std::collections::HashMap;
-use sxd_document::parser;
-use sxd_document::Package;
 
 #[derive(Debug)]
 pub(crate) struct CapsCache {
-    caps: Option<Package>,
+    caps: Option<String>,
 }
 
 impl CapsCache {
@@ -15,24 +13,19 @@ impl CapsCache {
         Self { caps: None }
     }
 
-    pub(crate) fn set(&mut self, capsxml: Option<&str>) -> VirtLintResult<()> {
-        self.caps = match capsxml {
-            Some(c) => Some(parser::parse(c)?),
-            None => None,
-        };
-        Ok(())
+    pub(crate) fn set(&mut self, capsxml: Option<String>) {
+        self.caps = capsxml
     }
 
     pub(crate) fn get(
         &mut self,
         conn: Option<&VirtLintConnect>,
         error_on_no_connect: bool,
-    ) -> VirtLintResult<Option<&Package>> {
+    ) -> VirtLintResult<Option<&String>> {
         if self.caps.is_none() {
             match conn {
                 Some(c) => {
                     let caps = c.conn.get_capabilities()?;
-                    let caps = parser::parse(&caps)?;
                     self.caps = Some(caps);
                 }
                 None => {
@@ -59,7 +52,7 @@ struct DomCapsKey {
 
 #[derive(Debug)]
 pub(crate) struct DomCapsCache {
-    cache: HashMap<DomCapsKey, Package>,
+    cache: HashMap<DomCapsKey, String>,
 }
 
 impl DomCapsCache {
@@ -73,9 +66,9 @@ impl DomCapsCache {
         self.cache.clear();
     }
 
-    pub(crate) fn add(&mut self, domcapsxml: &str) -> VirtLintResult<()> {
-        let domcapsxml_package = parser::parse(domcapsxml)?;
-        let domcapsxml_doc = domcapsxml_package.as_document();
+    pub(crate) fn add(&mut self, domcapsxml: String) -> VirtLintResult<()> {
+        let parser = Parser::default();
+        let domcapsxml_doc = parser.parse_string(&domcapsxml)?;
 
         let emulator = xpath_eval_or_none(&domcapsxml_doc, "//domainCapabilities/path");
         let arch = xpath_eval_or_none(&domcapsxml_doc, "//domainCapabilities/arch");
@@ -89,7 +82,7 @@ impl DomCapsCache {
             virttype: virttype.clone(),
         };
 
-        self.cache.insert(key, domcapsxml_package);
+        self.cache.insert(key, domcapsxml);
         Ok(())
     }
 
@@ -101,7 +94,7 @@ impl DomCapsCache {
         arch: Option<String>,
         machine: Option<String>,
         virttype: Option<String>,
-    ) -> VirtLintResult<Option<&Package>> {
+    ) -> VirtLintResult<Option<&String>> {
         let key = DomCapsKey {
             emulator: emulator.clone(),
             arch: arch.clone(),
@@ -131,8 +124,7 @@ impl DomCapsCache {
                     0,
                 )?;
 
-                let domcaps_package = parser::parse(&domcaps)?;
-                self.cache.insert(key.clone(), domcaps_package);
+                self.cache.insert(key.clone(), domcaps);
             }
         }
 

src/errors.rs
@@ -10,11 +10,20 @@ pub enum VirtLintError {
     Libvirt(#[from] virt::error::Error),
 
     #[error("Unable to parse XML: {0}")]
-    XMLParsing(#[from] sxd_document::parser::Error),
+    XMLParsing(#[from] libxml::parser::XmlParseError),
 
     #[error("Unknown validator tag: {0}")]
     UnknownValidatorTag(String),
 
     #[error("Invalid argument: {0}")]
     InvalidArgument(&'static str),
+
+    #[error("I/O error: {0}")]
+    IOError(String),
+
+    #[error("Lua error: {0}")]
+    LuaError(#[from] mlua::prelude::LuaError),
+
+    #[error(transparent)]
+    ParseIntError(#[from] std::num::ParseIntError),
 }

commit a5c599c47f93bb96391c80898b764d98577ca8fe

Update live ebuild
When bumping libvirt-rust crate (to consume Send and Sync traits
for libvirt objects), I've updated Cargo.toml but forgot to
bump the commit in the live ebuild

Fixes: 8a6e4e23d75bd34b2c1076f659e7dacc378d5043
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

virt-lint-9999.ebuild
@@ -63,7 +63,7 @@ CRATES="
 fi
 
 declare -A GIT_CRATES=(
-	[virt]="https://gitlab.com/libvirt/libvirt-rust.git;c7ee11c7585897f343d1c5ca9adc344bd9d21007;cargo_home/git/checkouts/libvirt-rust-a02c7639fc7cb7e7/c7ee11c/"
+	[virt]="https://gitlab.com/libvirt/libvirt-rust.git;b0506ed9cca960dd5f7679bca434a19d4b5cfc1d;cargo_home/git/checkouts/libvirt-rust-a02c7639fc7cb7e7/b0506ed/"
 )
 
 inherit edo cargo go-module